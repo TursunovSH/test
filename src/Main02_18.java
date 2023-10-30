b66a2c932b2ed5f0b59337fbfae800fec4b7cc4d
TursunovSH <149348256+TursunovSH@users.noreply.github.com>
2023-10-30 20:59:14 +0100

    make addition calculator

---
+++ src/Main02_18.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main02_18 {
+
+
+    public static int askNumber(String message, int min, int max) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(message);
+        int number;
+
+
+        do {
+            System.out.println(message);
+            number = scanner.nextInt();
+        } while (number < min || number > max);
+        return number;
+    }
+
+
+    public static void main(String[] args) {
+        int pin;
+        do {
+            pin = askNumber("Enter pin:", 1000,9999);
+            if (pin != 8956) {
+                System.out.println(" Incorrect pin");
+            } else {
+                break;
+            }
+
+        } while (true);
+    }
+}
