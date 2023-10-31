6e74e7dbd647779c6fa5ec31cb28ebb5743c744e
Shavkat Tursunov <149348256+TursunovSH@users.noreply.github.com>
2023-10-31 22:27:25 +0100

    add hello.java

--- src/Hello.java
+++ src/Hello.java
@@ -1,11 +1,5 @@
-import java.util.Scanner;
-
 public class Hello {
-
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        System.out.println( a + b);
+        System.out.println("Hello world");
     }
 }
